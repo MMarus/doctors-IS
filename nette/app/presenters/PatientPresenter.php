c691d05eed6102bce14410dbb1135fcfec8df0bd
Marek <marek.marusic@gmail.com>
2015-11-23 11:29:44 +0100

    patient

--- nette/app/presenters/PatientPresenter.php
+++ nette/app/presenters/PatientPresenter.php
@@ -8,9 +8,23 @@ use App\Model;
 
 class PatientPresenter extends BasePresenter
 {
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
-	}
+    private $db;
+    
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->db = $database;
+    }
+    
+    public function renderDefault()
+    {
+            $this->template->anyVariable = 'any value';
+            //$this->template->patient = $this->db->table('Pacient')->get($patientId);
+    }
+    
+    public function renderShow($id)
+    {
+            $this->template->id = $id;
+            $this->template->patient = $this->db->table('Pacient')->get($id);
+    }
 
 }
