6c33e65810affe6b40e45ebddb9059f431e312c0
Filip Ježovica <fjezovica@gmail.com>
2015-11-24 16:49:19 +0100

    ach ach ach ach ach ach ach ach ach ach ach

--- nette/app/presenters/HomepagePresenter.php
+++ nette/app/presenters/HomepagePresenter.php
@@ -4,6 +4,7 @@ namespace App\Presenters;
 
 use Nette;
 use App\Model;
+use Tracy\Debugger;
 
 
 class HomepagePresenter extends BasePresenter
@@ -18,7 +19,35 @@ class HomepagePresenter extends BasePresenter
 
 	public function renderDefault()
 	{
-		$plans = $this->db->query("SELECT P.ID as idp ,P.Planovany_datum as datum, C.Priezvisko as priez,C.Rodne_cislo as rc, V.Nazov  as vykon, P.Poznamky as pozn FROM Plan P JOIN Pacient C ON P.id_Pacient = C.ID JOIN VykonMaPlan VM ON VM.id_Plan = P.ID JOIN Vykon V ON VM.id_Vykon = V.ID WHERE P.id_NavstevaOrdinacie is NULL");
+		$idp 	= $this->db->query("SELECT P.ID as idp ,count(*) as cntx FROM Plan P JOIN VykonMaPlan VM ON VM.id_Plan = P.ID WHERE P.id_NavstevaOrdinacie is NULL group by P.ID");
+		$plans 	= $this->db->query("SELECT P.ID as idp ,P.Planovany_datum as datum, C.Priezvisko as priez,C.Rodne_cislo as rc, V.Nazov  as vykon, P.Poznamky as pozn FROM Plan P JOIN Pacient C ON P.id_Pacient = C.ID JOIN VykonMaPlan VM ON VM.id_Plan = P.ID JOIN Vykon V ON VM.id_Vykon = V.ID WHERE P.id_NavstevaOrdinacie is NULL");
+
+		$plans 	= $plans->fetchAll();
+		$idp	= $idp->fetchAll();
+
+		$xx = array();
+		$i = $x = 1;
+
+
+
+		foreach ($idp as $idpx)
+		{
+
+			foreach ($plans as $plansx)
+			{
+				if( $idpx->idp == $plansx->idp )
+				{
+					$xx[$idpx->idp][$x] = $plansx->vykon;
+					$x++;
+					$xx[$idpx->idp][0] = $plansx;
+				}
+			}$x=1;
+		}
+
+		Debugger::barDump($xx);
+
+
+
 		if (!$plans) {
 			$this->error('Stranka sa nenasla');
 		}
