487d9566e3ec153360c8fa5df001e8a35dcf797c
fjezovica <fjezovica@gmail.com>
2015-11-28 15:58:40 +0100

    OK MENU

--- nette/app/presenters/BasePresenter.php
+++ nette/app/presenters/BasePresenter.php
@@ -23,6 +23,11 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
         $this->template->username = $this->user->getIdentity()->meno . " " . $this->user->getIdentity()->priezvisko;
         $this->template->userinfo = $this->user->getIdentity();
 
+        $mm="";//default menu wrapper state
+        if (isset($_SESSION['menu_wrapper'])){$mm=$_SESSION['menu_wrapper'];}
+        else{$_SESSION['menu_wrapper'] = $mm;}
+        $this->template->menu_wrapper = $mm;
+
     }
 
 
